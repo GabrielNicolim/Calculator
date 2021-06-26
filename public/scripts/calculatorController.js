21a9522f0bb1638cb16b273e5557c1039d40f85b
Gabriel Nicolim <gabriel.gomes.nicolim@gmail.com>
2021-06-26 16:39:54 -0300

    Calculator reformulation

--- public/scripts/calculatorController.js
+++ public/scripts/calculatorController.js
@@ -6,21 +6,27 @@ class Calculator {
         this.outputResult = window.document.getElementById('number')
     }
 
+    clearAll() {
+        this.operation = []
+
+        this.showValues()
+    }
+
     inputNumber(num) {
         this.operation.push(num) 
         this.lastNumber = num
-        this.showResult()
+        this.showValues()
     }
 
     inputOperators(op) {
         if(op == '.') {
             if(this.lastOperator != '.' && this.operation.length > 0) {
 
-                if( this.operation[this.operation.length - 1] == '*' ||
-                    this.operation[this.operation.length - 1] == '%' ||
-                    this.operation[this.operation.length - 1] == '/' ||
-                    this.operation[this.operation.length - 1] == '-' ||
-                    this.operation[this.operation.length - 1] == '+'
+                if( this.operation[this.operation.length - 2] == '*' ||
+                    this.operation[this.operation.length - 2] == '%' ||
+                    this.operation[this.operation.length - 2] == '/' ||
+                    this.operation[this.operation.length - 2] == '-' ||
+                    this.operation[this.operation.length - 2] == '+'
                   )
                   {
                     this.operation.push('0')
@@ -33,34 +39,47 @@ class Calculator {
             }
         }
         else {
-            if( this.operation[this.operation.length - 1] == '*' ||
-                this.operation[this.operation.length - 1] == '%' ||
-                this.operation[this.operation.length - 1] == '/' ||
-                this.operation[this.operation.length - 1] == '-' ||
-                this.operation[this.operation.length - 1] == '+' ||
-                this.operation[this.operation.length - 1] == '.'  
+            if( this.operation[this.operation.length - 2] == '*' ||
+                this.operation[this.operation.length - 2] == '%' ||
+                this.operation[this.operation.length - 2] == '/' ||
+                this.operation[this.operation.length - 2] == '-' ||
+                this.operation[this.operation.length - 2] == '+' ||
+                this.operation[this.operation.length - 2] == '.'  
               ) 
             {
-                this.operation[this.operation.length - 1] = op
+                this.operation[this.operation.length - 2] = op
                 this.lastOperator = op
             }
-            else if(this.operation.length > 0){
+            else if(this.operation.length > 0) {
+                this.operation.push('<span class="operator">')
                 this.operation.push(op)
+                this.operation.push('</span>')
                 this.lastOperator = op
             }
         }
         
-        this.showResult()
+        this.showValues()
     }
 
     validCheck(value) {
         
     } 
 
+    showValues() {
+        if(this.operation.length == 0) {
+            this.outputResult.innerHTML = '0'
+        }
+        else {
+            this.outputResult.innerHTML = this.operation.toString().replaceAll(',', '')
+        }
+    }
+
     showResult() {
-        this.outputResult.innerText = this.operation.toString().replaceAll(',', '') 
+        //eval  
+
+        console.log(this.operation)
     }
 }
 
 var calc = new Calculator()
-calc.showResult()
+calc.showValues()
